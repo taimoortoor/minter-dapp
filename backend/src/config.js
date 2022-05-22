30799dc8993b87e551dfd7a80ae19ba5bfa31347
taimoortoor <52525806+taimoortoor@users.noreply.github.com>
2022-05-22 18:24:41 +0500

    updated contract address

--- backend/src/config.js
+++ backend/src/config.js
@@ -68,7 +68,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0x78ad43C427c5A4c2e0F0D1bdD424990cF792f4d6"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
